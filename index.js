fb8d11e194f985fae83b0c505806d21f1daa86e2
Aurelia Kariuki <kariukiaurelia5@gmail.com>
2025-11-20 16:22:43 +0300

    Implementing required javascrpt functions for budgeting lab

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-
-
-
-
+function calculateTax(amount){
+    let taxValue = amount * 0.1;
+    return taxValue;
+}
+function convertToUpperCase(text){
+    return text.toUpperCase()
+}
+function findMaximum(num1,num2){
+    return num1 > num2? num1:num2;
+}
+function isPalindrome(word){
+    const reversed = word.split('').reverse().join('');
+    return word ===reversed;
+}
+function calculateDiscountedPrice(originalPrice,discountedPercentage){
+    const discountAmount = (discountedPercentage/100)*originalPrice;
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
